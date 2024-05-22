e77545637b8993c59f8590d0ecff0beef66f2ad5
madhurd <madhurd@maveric-systems.com>
2024-05-22 11:01:23 +0530

    Add project files.

---
+++ Addtion/Program.cs
@@ -0,0 +1,10 @@
+﻿class Program
+{
+    private static void Main(string[] args)
+    {
+        int a = 20;
+        int b = 3;
+        int c = a + b;
+        System.Console.WriteLine("Addition: " + c);
+    }
+}
